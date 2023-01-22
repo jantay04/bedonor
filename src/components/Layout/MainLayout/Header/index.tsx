11a8c63719dcc016a172f2b30bd0323a9537b5c2
Adilet <baryktabasovadi@gmail.com>
2023-01-22 18:49:46 +0600

    added footer

--- src/components/Layout/MainLayout/Header/index.tsx
+++ src/components/Layout/MainLayout/Header/index.tsx
@@ -5,19 +5,23 @@ type Props = {};
 
 function Header({}: Props) {
   return (
-    <div className="container max-w-7xl mx-auto pt-6 flex flex-row items-center justify-between">
+    <div className="container max-w-7xl mx-auto p-4 pt-6 flex flex-row items-center justify-between">
       <img src={logosvg} alt="" className="w-logo cursor-pointer" />
       <div>
         <ul className="flex flex-row justify-between w-vsm text-ourblue ">
-          <button className="cursor-pointer focus:text-ourred">Главная</button>
-          <button className="cursor-pointer focus:text-ourred">
+          <button className="hover:text-ourred focus:text-ourred">
+            Главная
+          </button>
+          <button className="hover:text-ourred focus:text-ourred">
             Достижения
           </button>
-          <button className="cursor-pointer focus:text-ourred">
+          <button className="hover:text-ourred focus:text-ourred">
             Где сдать кровь?
           </button>
-          <button className="cursor-pointer focus:text-ourred">Новости</button>
-          <button className="cursor-pointer focus:text-ourred">FAQ</button>
+          <button className="hover:text-ourred focus:text-ourred">
+            Новости
+          </button>
+          <button className="hover:text-ourred focus:text-ourred">FAQ</button>
         </ul>
       </div>
       <button className="w-btn h-btn text-ourred font-semibold rounded-btn  outline outline-offset-0 outline-ourred">
