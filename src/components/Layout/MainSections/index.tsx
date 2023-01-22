bae1e93b6f105a5f5d9fbcc4eac30b6b38c32559
Bekbolot Zhantaev <45750628+bekbolot2004@users.noreply.github.com>
2023-01-22 10:26:30 +0100

    Done first mainsection

---
+++ src/components/Layout/MainSections/index.tsx
@@ -0,0 +1,28 @@
+// import Button from '@mui/material/Button/Button'
+// import Checkbox from '@mui/material/Checkbox/Checkbox'
+import React from 'react'
+// import pic1 from './assets/pic1.svg'
+
+type Props = {}
+
+function MainSections({ }: Props) {
+    return (
+        <div className='max-w-7xl mx-auto p-4 my-[200px]'>
+            <div className='grid grid-cols-12 gap-[32px] max-sm:gap-[10px] '>
+                <div className='col-span-7 max-lg:col-span-12 text-center'>
+                    <h1 className='text-[#2A5573] text-5xl font-bold max-md:text-4xl max-[520px]:text-2xl'>Спасай жизни, становясь донором крови</h1>
+                    <p className='hover:text-red-700 text-[#2A5573] text-lg  mt-9 font-medium'>Каждый день множество людей в Кыргызстане нуждаются
+                        в крови или ее компонентах для лечения или операций.
+                        Вы можете быть тем, кто спасет их жизнь.
+                        Присоединяйтесь к нам сегодня!</p>
+                    {/* <Button  variant="contained" color="secondary" size="large" sx={{ textTransform: "none", marginTop:'36px', borderRadius: '16px', fontSize: '14px', padding: '16px 40px' }}>Стать донором</Button> */}
+                </div>
+                <div className='col-span-5 max-lg:col-span-12 max-lg:order-first flex justify-center items-center '>
+                    {/* <img src={pic1} className='w-full' alt="graphic" /> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MainSections
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MainLayout from "../components/Layout/MainLayout";
+import MainSections from "../components/Layout/MainSections";
 
 type Props = {};
 
 const HomePage = (props: Props) => {
   return (
     <MainLayout>
-      <div>HomePage</div>
+      <MainSections/>
     </MainLayout>
   );
 };
