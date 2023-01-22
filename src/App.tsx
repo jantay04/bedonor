e8656e61c4dd6b452aa59114c9dd12ff07ddffff
Adilet <baryktabasovadi@gmail.com>
2023-01-22 21:44:00 +0600

    added map

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from "react";
-import MainLayout from "./components/Layout/MainLayout";
 import MainRoutes from "./MainRoutes";
-import Map from "./components/Layout/MainLayout/KgMapPart/Map";
-import Footer from "./components/Layout/MainLayout/Footer";
 
 function App() {
   //Map пока как комонент идет , потом в main добавил, это для удобста:)
   return (
     <div className="App">
       <MainRoutes />
-      <MainLayout>
-        {/* <Footer /> */}
-        <Map />
-      </MainLayout>
     </div>
   );
 }
